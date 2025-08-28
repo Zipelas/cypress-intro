4df94f6555c48626534dbfcc5eb675fbe937d166
Andreas Fagerlund <mcshaffa@gmail.com>
2025-08-28 11:15:32 +0200

    start Next:JS server

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,3 +1,4 @@
+import { spawn } from 'child_process';
 import { defineConfig } from 'cypress';
 import { MongoMemoryReplSet } from 'mongodb-memory-server';
 import { seedTodos } from './prisma/seed/todo';
@@ -10,7 +11,17 @@ export default defineConfig({
       const dbUri = db.getUri('cypress-test');
 
       // 2. Start Next.JS servern (på en annan port som ansluter till 1.)
-
+      const server = spawn(
+        'npx',
+        ['next', 'dev', '--turbopack', '-p', '3100'],
+        {
+          env: {
+            NODE_ENV: 'test',
+            DATABASE_URL: dbUri,
+          },
+          stdio: 'inherit',
+        }
+      );
       // 3. Vänta på att Next.JS servern är igång innan Cypress kör vidare.
       // 4. Städa upp processerna dvs Mongo databasen och Next.JS server
       // 5. Reseeda om databasen så att testerna blir oberoende av varandra
