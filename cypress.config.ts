5f6b8cd0d82aa135f5c6c105be319e84a70130d4
Andreas Fagerlund <mcshaffa@gmail.com>
2025-08-28 11:15:04 +0200

    start in memory mongo DB

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,21 @@
 import { defineConfig } from 'cypress';
-import { db } from './prisma/db';
+import { MongoMemoryReplSet } from 'mongodb-memory-server';
 import { seedTodos } from './prisma/seed/todo';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    async setupNodeEvents(on, config) {
+      // 1. Skapa en in-memory databas (replica set för att Prisma gnäller annars)
+      const db = await MongoMemoryReplSet.create({ replSet: { count: 1 } });
+      const dbUri = db.getUri('cypress-test');
+
+      // 2. Start Next.JS servern (på en annan port som ansluter till 1.)
+
+      // 3. Vänta på att Next.JS servern är igång innan Cypress kör vidare.
+      // 4. Städa upp processerna dvs Mongo databasen och Next.JS server
+      // 5. Reseeda om databasen så att testerna blir oberoende av varandra
+
       on('task', {
-        // 1. Skapa en in-memory databas (replica set för att Prisma gnäller annars)
-        // 2. Start Next.JS servern (på en annan port som ansluter till 1.)
-        // 3. Vänta på att Next.JS servern är igång innan Cypress kör vidare.
-        // 4. Städa upp processerna dvs Mongo databasen och Next.JS server
-        // 5. Reseeda om databasen så att testerna blir oberoende av varandra
         async reseed() {
           await db.todo.deleteMany();
           await seedTodos();
