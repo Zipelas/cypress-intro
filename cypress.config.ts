a79da80b71f4d0fb9a0def01a76f9528acb608b3
Andreas Fagerlund <mcshaffa@gmail.com>
2025-08-28 11:22:00 +0200

    wait on Next.js server before running tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { spawn } from 'child_process';
 import { defineConfig } from 'cypress';
 import { MongoMemoryReplSet } from 'mongodb-memory-server';
+import waitOn from 'wait-on';
 import { seedTodos } from './prisma/seed/todo';
 
 export default defineConfig({
@@ -23,6 +24,7 @@ export default defineConfig({
         }
       );
       // 3. Vänta på att Next.JS servern är igång innan Cypress kör vidare.
+      await waitOn({ resources: [`http://localhost:3100`], timeout: 60_000 });
       // 4. Städa upp processerna dvs Mongo databasen och Next.JS server
       // 5. Reseeda om databasen så att testerna blir oberoende av varandra
 
