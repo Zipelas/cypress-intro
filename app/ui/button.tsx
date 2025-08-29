e26741b8e3dbeb11f97d2be8e981f0813c0db011
Andreas Fagerlund <mcshaffa@gmail.com>
2025-08-29 16:40:13 +0200

    removed Link. Added PropsWithChildren

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -1,15 +1,14 @@
 'use client';
 
 import Link from 'next/link';
+import { PropsWithChildren } from 'react';
 
 // interface
 
-export default function Button() {
+export default function Button(props: PropsWithChildren) {
   return (
-    <Link href='/statistics'>
-      <button className=' text-sky-600 text-2xl border-4 border-sky-600 rounded-4xl p-'>
-        See statistics
+      <button className=' text-sky-600 text-2xl border-4 border-sky-600 rounded-4xl p-2'>
+        { props.children }
       </button>
-    </Link>
   );
 }
