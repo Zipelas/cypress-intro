74bb15ee25fddca49b59e360798e5247a5e8a13d
Andreas Fagerlund <mcshaffa@gmail.com>
2025-08-29 19:04:03 +0200

    added div

--- app/ui/infocard.tsx
+++ app/ui/infocard.tsx
@@ -4,5 +4,9 @@ import { PropsWithChildren } from 'react';
 
 export default function InfoCard(props: PropsWithChildren) {
   return (
+    <div className=' text-sky-600 text-2xl border-4 border-sky-600 rounded-4xl p-2 m-2'>
+      TEST
+      {props.children}
+    </div>
   );
 }
