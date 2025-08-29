8207f87ec82bc113220060fd7898580c43b57201
Andreas Fagerlund <mcshaffa@gmail.com>
2025-08-29 15:22:51 +0200

    Added a input field

---
+++ app/ui/input.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+// import
+
+// interface
+
+export default function Input() {
+  return (
+    <input
+      type='text'
+      placeholder='Enter how much you have walked'
+      className='border-2 border-sky-600 rounded-4xl p-2 text-slate-50 bg-black'
+    />
+  );
+}
