948e31b697254cf76de44009da7049eadb86e216
Andreas Fagerlund <mcshaffa@gmail.com>
2025-08-29 18:40:30 +0200

    added Dropdown and import

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link';
 import Button from '../ui/button';
+import Dropdown from '../ui/dropdown';
 
 export default async function Statistics() {
   return (
     <div>
       <h1 className='text-6xl text-sky-600'>Statistics</h1>
+      <Dropdown />
       <Link href='/'>
         <Button>↩ Back</Button>
       </Link>
