3be1eb59d86f03203c42d47a8dea6f96aa516ce5
Andreas Fagerlund <mcshaffa@gmail.com>
2025-08-29 15:17:59 +0200

    added test cy.visit and cy.get should exitst

--- cypress/e2e/input.cy.ts
+++ cypress/e2e/input.cy.ts
@@ -2,4 +2,9 @@ beforeEach(() => {
   cy.task('reseed');
 });
 
+it('should display input field', () => {
+  cy.visit('/');
+  cy.get('input').should('exist');
+    // cy.get('input').should('have.attr', 'placeholder', 'Enter a new task');
 
+});
