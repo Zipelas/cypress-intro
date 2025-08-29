e18d40cbd2aa96270706c0871dd0935dba02d797
Andreas Fagerlund <mcshaffa@gmail.com>
2025-08-29 16:39:17 +0200

    added Link and imports

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,3 +1,13 @@
+import Link from 'next/link';
+import Button from '../ui/button';
+
 export default async function Statistics() {
-  return <h1 className='text-6xl text-sky-600'>Statistics</h1>;
+  return (
+    <div>
+      <h1 className='text-6xl text-sky-600'>Statistics</h1>
+      <Link href='/'>
+        <Button>↩ Back</Button>
+      </Link>
+    </div>
+  );
 }
