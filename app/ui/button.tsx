819a5cca34d0b041949af7821cb80c36e92c5779
Andreas Fagerlund <mcshaffa@gmail.com>
2025-08-29 17:26:59 +0200

    removed import

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -1,14 +1,13 @@
 'use client';
 
-import Link from 'next/link';
 import { PropsWithChildren } from 'react';
 
 // interface
 
 export default function Button(props: PropsWithChildren) {
   return (
-      <button className=' text-sky-600 text-2xl border-4 border-sky-600 rounded-4xl p-2'>
-        { props.children }
-      </button>
+    <button className=' text-sky-600 text-2xl border-4 border-sky-600 rounded-4xl p-2'>
+      {props.children}
+    </button>
   );
 }
