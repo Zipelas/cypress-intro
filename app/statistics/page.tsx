6c63b0b84fa379aa8d4506c1ceb05f720ce92f17
Andreas Fagerlund <mcshaffa@gmail.com>
2025-08-29 19:03:50 +0200

    added inford and import

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import Button from '../ui/button';
 import Dropdown from '../ui/dropdown';
-
+import InfoCard from '../ui/infocard';
 export default async function Statistics() {
   return (
     <div>
       <h1 className='text-6xl text-sky-600'>Statistics</h1>
       <Dropdown />
+      <InfoCard />
       <Link href='/'>
         <Button>↩ Back</Button>
       </Link>
