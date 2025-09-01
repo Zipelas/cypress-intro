827e4771315e166af081069d9a61cdc284e92ab5
Andreas Fagerlund <mcshaffa@gmail.com>
2025-09-01 11:10:21 +0200

    deleted file

--- cypress/e2e/input.cy.ts
+++
@@ -1,9 +0,0 @@
-beforeEach(() => {
-  cy.task('reseed');
-});
-
-it('should display input field', () => {
-  cy.visit('/');
-  cy.get('input').should('exist');
-    // cy.get('input').should('have.attr', 'placeholder', 'Enter a new task');
-});
