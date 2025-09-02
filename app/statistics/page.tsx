11a55963367d0e22b263ef4b065f835738a724ed
Andreas Fagerlund <mcshaffa@gmail.com>
2025-09-02 19:32:54 +0200

    added StatForm & removed components

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,16 +1,10 @@
-import Link from 'next/link';
-import Button from '../ui/button';
-import Dropdown from '../ui/dropdown';
-import InfoCard from '../ui/infocard';
+import StatsForm from '../ui/stats-form';
+
 export default async function Statistics() {
   return (
-    <div>
+    <>
       <h1 className='text-6xl text-sky-600'>Statistics</h1>
-      <Dropdown />
-      <InfoCard />
-      <Link href='/'>
-        <Button>↩ Back</Button>
-      </Link>
-    </div>
+      <StatsForm />
+    </>
   );
 }
