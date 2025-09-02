e69250321b37fb75fc4e80ae1d6ceed7f1cac865
Andreas Fagerlund <mcshaffa@gmail.com>
2025-09-02 19:40:44 +0200

    added describe

--- cypress/e2e/stats-form.cy.tsx
+++ cypress/e2e/stats-form.cy.tsx
@@ -2,12 +2,13 @@ beforeEach(() => {
   cy.task('reseed');
   cy.visit('/');
 });
-
-it('should display dropdown', () => {
-  cy.get('select').should('exist');
-});
-
-it('should display infoCard', () => {
-  cy.visit('/');
-  cy.get('div').should('exist');
+describe('StatsForm', () => {
+  it('should display infoCard', () => {
+    cy.visit('/');
+    cy.get('div').should('exist');
+  });
+  
+  it('should display dropdown', () => {
+    cy.get('select').should('exist');
+  });
 });
