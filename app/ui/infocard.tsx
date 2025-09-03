19edafd3eb90f5992ded258c49dd10f081e60511
Andreas Fagerlund <mcshaffa@gmail.com>
2025-09-03 14:53:50 +0200

    added type and edit infocard

--- app/ui/infocard.tsx
+++ app/ui/infocard.tsx
@@ -1,14 +1,21 @@
 'use client';
 
-import { PropsWithChildren } from 'react';
+type InfoCardProps = {
+  choice: string; // t.ex. "avg" | "monthly" | "yearly"
+  label: string; // human readable text
+};
 
-export default function InfoCard(props: PropsWithChildren) {
+export default function InfoCard({ choice, label }: InfoCardProps) {
   return (
     <div
       data-cy='infocard'
-      className=' text-sky-600 text-2xl border-4 border-sky-600 rounded-4xl p-2 m-2'>
-      InfoCard Statistics
-      {props.children}
+      className=' text-sky-600 text-2xl border-4 border-sky-600 rounded-xl p-4 m-2'>
+      <h3 className='text-xl font-semibold'>Valt filter</h3>
+      {choice ? (
+        <p className='mt-2'>{label}</p>
+      ) : (
+        <p className='mt-2 italic text-gray-500'>Välj något i dropdownen</p>
+      )}
     </div>
   );
 }
