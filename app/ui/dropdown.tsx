42c740e6851f9d6e78817ded59ad2659aa3c2360
Andreas Fagerlund <mcshaffa@gmail.com>
2025-09-03 14:54:37 +0200

    added type. edit function & select

--- app/ui/dropdown.tsx
+++ app/ui/dropdown.tsx
@@ -2,17 +2,27 @@
 
 import { PropsWithChildren } from 'react';
 
-export default function Dropdown(props: PropsWithChildren) {
+type DropdownProps = PropsWithChildren<{
+  value: string;
+  onChange: (v: string) => void;
+  name?: string;
+}>;
+
+export default function Dropdown({
+  value,
+  onChange,
+  children,
+  name,
+}: DropdownProps) {
   return (
     <div>
       <select
         data-cy='dropdown'
-        className=' text-sky-600 text-2xl border-4 border-sky-600 rounded-4xl p-2'>
-        <option value=''>Select an option</option>
-        <option value='1'>Gått i snitt</option>
-        <option value='2'>Gått varje månad</option>
-        <option value='3'>Gått totalt per år</option>
-        {props.children}
+        name={name}
+        className=' text-sky-600 text-2xl border-4 border-sky-600 rounded-4xl p-2'
+        value={value}
+        onChange={(e) => onChange(e.target.value)}>
+        {children}
       </select>
     </div>
   );
