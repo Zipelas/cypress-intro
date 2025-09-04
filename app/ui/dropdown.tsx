6a994b929e277e7ca2a0a3002372737bf6e0de17
Andreas Fagerlund <mcshaffa@gmail.com>
2025-09-04 12:53:56 +0200

    redid function & add style

--- app/ui/dropdown.tsx
+++ app/ui/dropdown.tsx
@@ -5,25 +5,16 @@ import { PropsWithChildren } from 'react';
 type DropdownProps = PropsWithChildren<{
   value: string;
   onChange: (v: string) => void;
-  name?: string;
 }>;
 
-export default function Dropdown({
-  value,
-  onChange,
-  children,
-  name,
-}: DropdownProps) {
+export default function Dropdown({ value, onChange, children }: DropdownProps) {
   return (
-    <div>
-      <select
-        data-cy='dropdown'
-        name={name}
-        className=' text-sky-600 text-2xl border-4 border-sky-600 rounded-4xl p-2'
-        value={value}
-        onChange={(e) => onChange(e.target.value)}>
-        {children}
-      </select>
-    </div>
+    <select
+      data-cy='dropdown'
+      className='text-sky-600 text-2xl border-4 border-sky-600 rounded-3xl p-2'
+      value={value}
+      onChange={(e) => onChange(e.target.value)}>
+      {children}
+    </select>
   );
 }
