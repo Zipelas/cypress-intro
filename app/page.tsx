65940b32a8308f47e51bcfbf5ac9ddfda5c20f2b
Andreas Fagerlund <mcshaffa@gmail.com>
2025-09-04 12:51:07 +0200

    removed Link. added statsform. add style

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { db } from '@/prisma/db';
-import Link from 'next/link';
-import Button from './ui/button';
+import StatsForm from './ui/stats-form';
 import TodoList from './ui/todo-list';
 import WalkForm from './ui/walk-form';
 
@@ -8,39 +7,15 @@ export default async function Home() {
   const todos = await db.todo.findMany();
 
   return (
-    <main className='bg-black text-sky-600 p-4'>
-      <h1 className='text-6xl text-sky'>🚶‍♂️‍➡️Walk Tracker - Walk this way 🎶</h1>
-      <WalkForm />
-      <Link href='/statistics'>
-        <Button>Statistics</Button>
-      </Link>
+    <main className='bg-black text-sky-600'>
+      <h1 className='text-6xl text-sky p-4'>
+        🚶‍♂️‍➡️Walk Tracker - Walk this way 🎶
+      </h1>
+      <div className='text-6xl text-sky-600 p-6 grid md:grid-cols-2 gap-6'>
+        <WalkForm />
+        <StatsForm />
+      </div>
       <TodoList defaultTodos={todos} />
     </main>
   );
 }
-
-// "use client";
-// import { todos as initialTodos } from "./data";
-// import { useState } from "react";
-
-// export default function Page() {
-//   const [todos, setTodos] = useState(initialTodos);
-
-//   function handleDelete(index: number) {
-//     setTodos((prev) => prev.filter((_, i) => i !== index));
-//   }
-
-// export default function Home() {
-//   return (
-//     <main>
-//       <ul>
-//         {todos.map((todo) => (
-//           <li key={todo}>
-//             {todo}
-//             <button onClick={() => onDelete(todo)}>Delete</button>
-//           </li>
-//         ))}
-//       </ul>
-//     </main>
-//   );
-// }
