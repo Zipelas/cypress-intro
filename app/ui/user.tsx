728f26b3c6cde5cb6e4113388c61e4b5f295151e
Zipelas <mcshaffa@gmail.com>
2025-09-24 18:39:00 +0200

    created user with interface & input

---
+++ app/ui/user.tsx
@@ -0,0 +1,19 @@
+interface UserProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export default function User({ value, onChange }: UserProps) {
+  return (
+    <div>
+      <input
+        placeholder='Name'
+        type='text'
+        id='username'
+        name='username'
+        value={value}
+        onChange={e => onChange(e.target.value)}
+      />
+    </div>
+  );
+}
