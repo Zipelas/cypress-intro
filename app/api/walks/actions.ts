3e7e41e9ff4d7795d23029a98881f62c76d1c763
Zipelas <mcshaffa@gmail.com>
2025-10-01 15:48:05 +0200

    added getWalks

--- app/api/walks/actions.ts
+++ app/api/walks/actions.ts
@@ -1,9 +1,17 @@
-"use server";
+'use server';
 
-import { db } from "@/prisma/db";
+import { db } from '@/prisma/db';
 
-export async function saveWalk(data: { date: string; amount: number }) {
+export async function saveWalk(data: {
+  date: string;
+  amount: number;
+  text: string;
+}) {
   await db.walk.create({
     data,
   });
+}
+
+export async function getWalks() {
+  return await db.walk.findMany({});
 }
