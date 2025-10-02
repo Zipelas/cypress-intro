b4ec609133b6e4985ba7a9c944a790047e7dd0b0
Zipelas <mcshaffa@gmail.com>
2025-10-02 16:52:24 +0200

    added disabled to dropdown props

--- app/ui/dropdown.tsx
+++ app/ui/dropdown.tsx
@@ -5,14 +5,23 @@ import { PropsWithChildren } from 'react';
 type DropdownProps = PropsWithChildren<{
   value: string;
   onChange: (v: string) => void;
+  disabled?: boolean;
 }>;
 
-export default function Dropdown({ value, onChange, children }: DropdownProps) {
+export default function Dropdown({
+  value,
+  onChange,
+  disabled = false,
+  children,
+}: DropdownProps) {
   return (
     <select
       data-cy='dropdown'
-      className='text-sky-600 text-2xl border-4 border-sky-600 rounded-3xl p-2'
+      className={`text-sky-600 text-2xl border-4 border-sky-600 rounded-3xl p-2 ${
+        disabled ? 'opacity-50 cursor-not-allowed' : ''
+      }`}
       value={value}
+      disabled={disabled}
       onChange={(e) => onChange(e.target.value)}>
       {children}
     </select>
