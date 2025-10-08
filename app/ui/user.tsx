0f9844f948803d8c5e11343ea04b3c24ede816b8
Zipelas <mcshaffa@gmail.com>
2025-10-08 16:05:36 +0200

    added data-cy & className

--- app/ui/user.tsx
+++ app/ui/user.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client';
 
 interface UserProps {
   value: string;
@@ -9,12 +9,14 @@ export default function User({ value, onChange }: UserProps) {
   return (
     <div>
       <input
+        data-cy='user-input'
+        className='w-full px-3 py-2 border rounded'
         placeholder='Name'
         type='text'
         id='username'
         name='username'
         value={value}
-        onChange={e => onChange(e.target.value)}
+        onChange={(e) => onChange(e.target.value)}
       />
     </div>
   );
